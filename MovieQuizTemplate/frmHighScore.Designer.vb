2dec53f87730989b38605f05d98b0f3acb7df208
edward jacques <e.jacques@wlc.ac.uk>
2023-04-17 12:17:48 +0100

    High Score form designed

--- MovieQuizTemplate/frmHighScore.Designer.vb
+++ MovieQuizTemplate/frmHighScore.Designer.vb
@@ -22,9 +22,71 @@ Partial Class frmHighScore
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LlstHighScore = New System.Windows.Forms.ListBox()
+        Me.btnAgain = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(59, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(683, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Congratulations Your Score Will Be Added To The Hall Of Fame"
+        Me.Label1.UseWaitCursor = True
+        '
+        'LlstHighScore
+        '
+        Me.LlstHighScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LlstHighScore.FormattingEnabled = True
+        Me.LlstHighScore.ItemHeight = 24
+        Me.LlstHighScore.Location = New System.Drawing.Point(115, 110)
+        Me.LlstHighScore.Name = "LlstHighScore"
+        Me.LlstHighScore.Size = New System.Drawing.Size(332, 148)
+        Me.LlstHighScore.TabIndex = 1
+        '
+        'btnAgain
+        '
+        Me.btnAgain.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAgain.Location = New System.Drawing.Point(559, 340)
+        Me.btnAgain.Name = "btnAgain"
+        Me.btnAgain.Size = New System.Drawing.Size(162, 42)
+        Me.btnAgain.TabIndex = 2
+        Me.btnAgain.Text = "Again"
+        Me.btnAgain.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuit.Location = New System.Drawing.Point(560, 404)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(160, 38)
+        Me.btnQuit.TabIndex = 3
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'frmHighScore
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnAgain)
+        Me.Controls.Add(Me.LlstHighScore)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmHighScore"
         Me.Text = "frmHighScore"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LlstHighScore As ListBox
+    Friend WithEvents btnAgain As Button
+    Friend WithEvents btnQuit As Button
 End Class

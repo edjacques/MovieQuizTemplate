2a995374bd96592234458f84ad02f9c226f7abec
edward jacques <e.jacques@wlc.ac.uk>
2023-04-17 12:01:31 +0100

    Question 3 page complete

--- MovieQuizTemplate/frmQuestion3.vb
+++ MovieQuizTemplate/frmQuestion3.vb
@@ -1,3 +1,9 @@
 ﻿Public Class frmQuestion3
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
 
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+
+    End Sub
 End Class

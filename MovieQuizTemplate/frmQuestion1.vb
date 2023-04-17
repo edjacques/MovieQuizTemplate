603bfb405d7e9f57a0c1aad5a98ef94843e2da2b
edward jacques <e.jacques@wlc.ac.uk>
2023-04-17 11:43:57 +0100

    First question page completed

--- MovieQuizTemplate/frmQuestion1.vb
+++ MovieQuizTemplate/frmQuestion1.vb
@@ -2,4 +2,8 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles btnAnswer1.CheckedChanged
+
+    End Sub
 End Class

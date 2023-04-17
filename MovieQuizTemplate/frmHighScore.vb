63f96c943b6626ab20b50b4ed98dedbfedaeeb48
edward jacques <e.jacques@wlc.ac.uk>
2023-04-17 13:55:40 +0100

    Highscore working

--- MovieQuizTemplate/frmHighScore.vb
+++ MovieQuizTemplate/frmHighScore.vb
@@ -1,9 +1,20 @@
 ﻿Public Class frmHighScore
-    Private Sub LlstHighScore_SelectedIndexChanged(sender As Object, e As EventArgs) Handles LlstHighScore.SelectedIndexChanged
-        playerScore = 0
-        playerName = txtName.Text
-        frmQuestion1.Show()
+    Public Sub doScore()
+        If (playerScore < 1) Then
+            lblMessage.Text = "Sorry your score was not good enough"
+        Else
+            ilbMessage.Text = "Congratulations your score will be added to the Hall of Fame"
+1StHighScore.Items.Add(playerName & vbTab &vbTab & playerScore)
+           
+        End If
+    End Sub
 
+    Private Sub btnAgain_Click(sender As Object, e As EventArgs) Handles btnAgain.Click
+        frmMain.Show()
         Me.Hide()
+    End Sub
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        Application.Exit()
     End Sub
 End Class
--- MovieQuizTemplate/frmQuestion3.vb
+++ MovieQuizTemplate/frmQuestion3.vb
@@ -6,4 +6,15 @@
     Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
 
     End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        If (btnAnswer1.Checked) Then
+            playerScore = playerScore + 1
+        End If
+
+        frmHighScore.Show()
+        frmHighScore.doScore()
+
+        Me.Hide()
+    End Sub
 End Class

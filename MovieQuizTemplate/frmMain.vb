604c46bed21baae3f7d9fd847ba8c3299a803aea
edward jacques <e.jacques@wlc.ac.uk>
2023-04-17 12:34:57 +0100

    code add to main form

--- MovieQuizTemplate/frmMain.vb
+++ MovieQuizTemplate/frmMain.vb
@@ -1,13 +1,9 @@
 ﻿Public Class frmMain
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
-    End Sub
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
 
     Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
-
+        playerScore = 0
+        playerName = txtName.Text
+        frmQuestion1.Show()
     End Sub
 End Class
